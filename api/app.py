57ac30b1968454146a34dc3b161c8f9c6d3abe53
merkur-spec <merkurgj@proton.me>
2025-11-06 03:04:32 -0500

    Update app.py

--- api/app.py
+++ api/app.py
@@ -104,6 +104,8 @@ def can_add_move(combo, move, body_punch_used, target_length):
             return False
         if (prev == "rear cross" and move == "rear cross"):
             return False
+        if (prev == "rear cross" and move == "rear body cross") or (prev == "rear body cross" and move == "rear cross"):
+            return False
 
         # Only allow defensive moves and offensive moves to be done consecutively from the same side
         if is_defensive(prev) and is_punch(move):
